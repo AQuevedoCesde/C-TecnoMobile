75d0f4c7681361fb6994899f9d28c05b35f4f67e
AndresQuevedo <aquevedocede23@gmail.com>
2024-05-26 13:57:37 -0500

    countDownTimer incluido

--- app/src/main/java/com/example/c_tecno_mobile/MainActivity.java
+++ app/src/main/java/com/example/c_tecno_mobile/MainActivity.java
@@ -11,7 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CountDownTimer timer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
