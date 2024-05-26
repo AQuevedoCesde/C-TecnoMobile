08e6f3d3e5af59f32d5eeb6f614d7a63e7cc30d0
AndresQuevedo <aquevedocede23@gmail.com>
2024-05-26 14:37:15 -0500

    Splash C-T

--- app/src/main/java/com/example/c_tecno_mobile/MainActivity.java
+++ app/src/main/java/com/example/c_tecno_mobile/MainActivity.java
@@ -18,6 +18,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        timer = new CountDownTimer(3000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        };
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        timer.start();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        timer.cancel();
     }
 }
-
