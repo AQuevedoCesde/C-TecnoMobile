7db033ea6ed4661fadace8935ca78643d926c527
AndresQuevedo <aquevedocede23@gmail.com>
2024-05-27 18:03:30 -0500

    Crear usuario Completad. Funcional

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-database")
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
     testImplementation(libs.junit)
