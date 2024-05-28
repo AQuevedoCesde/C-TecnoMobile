2320773b2a79b942db140f906c40328bc54b7a56
AndresQuevedo <aquevedocede23@gmail.com>
2024-05-28 01:08:20 -0500

    Servicio conectados a los Layouts

--- app/src/main/java/com/example/c_tecno_mobile/PaginaPrincipal.java
+++ app/src/main/java/com/example/c_tecno_mobile/PaginaPrincipal.java
@@ -1,24 +1,56 @@
 package com.example.c_tecno_mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
 
 public class PaginaPrincipal extends AppCompatActivity {
 
+    CardView computadores, salones, accesorios, reserva;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_pagina_principal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        computadores = findViewById(R.id.cardComputador);
+        salones = findViewById(R.id.cardSalones);
+        accesorios = findViewById(R.id.cardAccesorios);
+        reserva = findViewById(R.id.cardReserva);
+
+        computadores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Computadores.class);
+                v.getContext().startActivity(intent);
+            }
         });
+        salones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Salones.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+        accesorios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Accesorios.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+        reserva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Reserva.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+
     }
 }
