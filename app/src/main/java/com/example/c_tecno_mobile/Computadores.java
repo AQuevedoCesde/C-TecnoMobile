71c1b7312632554edc573eaaa2f7f95ad07dbbb2
AndresQuevedo <aquevedocede23@gmail.com>
2024-05-28 17:59:07 -0500

    Pagina principal Front end Terminada

--- app/src/main/java/com/example/c_tecno_mobile/Computadores.java
+++ app/src/main/java/com/example/c_tecno_mobile/Computadores.java
@@ -1,6 +1,9 @@
 package com.example.c_tecno_mobile;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,7 @@ public class Computadores extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_computadores);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
+
 }
